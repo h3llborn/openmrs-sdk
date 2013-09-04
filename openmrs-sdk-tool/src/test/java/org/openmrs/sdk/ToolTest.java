82cd31bb991cdc38b45f83b8aa44b460b28ffea2
h3llborn <h3llborn@openmrs.org>
2013-09-04 08:39:11 -0400

    SDK-11: adjusted ToolTest.java; added Tool.jar to /bin

--- openmrs-sdk-tool/src/test/java/org/openmrs/sdk/ToolTest.java
+++ openmrs-sdk-tool/src/test/java/org/openmrs/sdk/ToolTest.java
@@ -16,7 +16,7 @@ public class ToolTest {
 		Element element = tool.updateArtifactItem(document,"org.openmrs.module", "logic", "123456789");
 
 		Assert.assertThat(element, CoreMatchers.is(CoreMatchers.notNullValue()));
-		Assert.assertThat(element.asXML(), CoreMatchers.containsString("123456789"));
+		Assert.assertThat(element.asXML(), CoreMatchers.containsString("logic-omod"));
 	}
 
 }
